e98f71ad7a42f9ffed4a3ae795854ac4f5467352
Kwangjin <kwangjin5468@naver.com>
2024-01-02 00:55:20 +0900

    Feat : React + SpringBoot 연동 작업

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    fetch('/api/hello')
+        .then(response => response.text())
+        .then(message => {
+          setData(message);
+        });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <p>{data}</p>
+        </header>
+      </div>
   );
 }
 
