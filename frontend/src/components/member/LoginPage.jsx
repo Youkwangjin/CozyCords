f296ee7e9e75d030d2556890027f58fd3a1b2e3d
Kwangjin <kwangjin5468@naver.com>
2024-01-09 18:07:31 +0900

    Feat : Header ccs 추가 작업

--- frontend/src/components/member/LoginPage.jsx
+++ frontend/src/components/member/LoginPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import './MemberPage.css';
+import axios from 'axios';
 
 export default function LoginPage() {
     const [user_id, setUserId] = useState('');
@@ -38,48 +39,69 @@ export default function LoginPage() {
         setNotAllow(true);
     }, [useridValid, passwordValid]);
 
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        if (!notAllow) {
+            try {
+                const response = await axios.post('http://localhost:8080/api/login', {
+                    userId: user_id,
+                    password: user_pwd,
+                });
+
+                // 로그인 성공, Jwt를 로컬 스토리지에 저장
+                const { token } = response.data;
+                localStorage.setItem('userToken', token);
+                window.location.href = '/';
+            } catch (error) {
+                console.error("로그인 실패", error);
+            }
+        }
+    };
+
 
     return (
         <div className="page">
-            <div className="titleWrap">로그인</div>
+            <form onSubmit={handleLogin}>
+                <div className="titleWrap">로그인</div>
 
-            <div className="contentWrap">
-                <div className="inputTitle">아이디</div>
-                <div className="inputWrap">
-                    <input
-                        className="input"
-                        name="user_id"
-                        value={user_id}
-                        placeholder="abcd1234"
-                        onChange={handleUserId}/>
+                <div className="contentWrap">
+                    <div className="inputTitle">아이디</div>
+                    <div className="inputWrap">
+                        <input
+                            className="input"
+                            name="user_id"
+                            value={user_id}
+                            placeholder="abcd1234"
+                            onChange={handleUserId}/>
+                    </div>
+                    <div className="errorMessageWrap">
+                        {
+                            !useridValid && user_id.length > 0 && (
+                                <div>올바른 아이디를 입력해주세요.</div>
+                            )
+                        }
+                    </div>
+                    <div style={{marginTop: "26px"}} className="inputTitle">비밀번호</div>
+                    <div className="inputWrap">
+                        <input
+                            className="input"
+                            name="user_pwd"
+                            value={user_pwd}
+                            placeholder="영문, 숫자, 특수문자 포함 8자 이상"
+                            onChange={handleUserPwd}/>
+                    </div>
+                    <div className="errorMessageWrap">
+                        {
+                            !passwordValid && user_pwd.length > 0 && (
+                                <div>올바른 비밀번호를 입력해주세요.</div>
+                            )
+                        }
+                    </div>
+                    <div>
+                        <button disabled={notAllow} onClick={handleLogin} className="bottomButton">로그인</button>
+                    </div>
                 </div>
-                <div className="errorMessageWrap">
-                    {
-                        !useridValid && user_id.length > 0 && (
-                            <div>올바른 아이디를 입력해주세요.</div>
-                        )
-                    }
-                </div>
-                <div style={{marginTop: "26px"}} className="inputTitle">비밀번호</div>
-                <div className="inputWrap">
-                    <input
-                        className="input"
-                        name="user_pwd"
-                        value={user_pwd}
-                        placeholder="영문, 숫자, 특수문자 포함 8자 이상"
-                        onChange={handleUserPwd}/>
-                </div>
-                <div className="errorMessageWrap">
-                    {
-                        !passwordValid && user_pwd.length > 0 && (
-                            <div>올바른 비밀번호를 입력해주세요.</div>
-                        )
-                    }
-                </div>
-                <div>
-                    <button disabled={notAllow} className="bottomButton">확인</button>
-                </div>
-            </div>
+            </form>
         </div>
-    )
+)
 }
