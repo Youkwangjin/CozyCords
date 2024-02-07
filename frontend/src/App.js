f34b14d316b1e480c7bf47706b5c3b56c5614c67
Kwangjin <kwangjin5468@naver.com>
2024-02-07 19:03:12 +0900

    Feat : 관리자 페이지 수정 + jwt-decode 라이브러리 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,6 @@ import LoginPage from './components/member/LoginPage';
 import SignUpPage from './components/member/SignUpPage';
 import MyUpdatePage from './components/member/MyUpdatePage';
 import MyDeletePage from './components/member/MyDeletePage';
-import AdminLoginPage from "./components/admin/AdminLoginPage";
 
 function App() {
     return (
@@ -18,8 +17,6 @@ function App() {
                     <Routes>
                         {/* 메인 홈페이지 라우트 */}
                         <Route path="/" element={<WithHeader><IndexPage /></WithHeader>} />
-                        {/* 관리자 페이지 라우트 */}
-                        <Route path="/admin/login" element={<AdminLoginPage />} />
                         {/* 사용자 라우트 */}
                         <Route path="/login" element={<WithHeader><LoginPage /></WithHeader>} />
                         <Route path="/signup" element={<WithHeader><SignUpPage /></WithHeader>} />
---
+++ frontend/src/components/admin/AdminPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AdminHeader from './AdminHeader';
+
+function AdminPage() {
+    return (
+        <div>
+            <AdminHeader />
+            <h1>관리자 페이지</h1>
+        </div>
+    );
+}
+
+export default AdminPage;
